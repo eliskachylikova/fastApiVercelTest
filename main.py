0f889daf69892c2366635eb18c479b630606e83e
Eliška Chylíková <xchyliko@mendelu.cz>
2024-05-18 11:09:32 +0200

    Main.py

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
+import httpx
 from fastapi import FastAPI
 
 app = FastAPI()
+
+limits = httpx.Limits(max_keepalive_connections=5, max_connections=10)
+timeout = httpx.Timeout(timeout=5.0, read=15.0)
+client = httpx.AsyncClient(limits=limits, timeout=timeout)
+
+
+@app.on_event("shutdown")
+async def shutdown_event():
+    print("shutting down...")
+    await client.aclose()
 
 
 @app.get("/")
